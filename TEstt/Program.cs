19cd6ca379dd5a25f850be41f8890b6d03909fd8
THH THH <lingyr@yandex.ru>
2022-01-14 02:42:31 +0300

    Add project files.

---
+++ TEstt/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("https://tester.consimple.pro/");
+
+
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+
+
+Console.WriteLine($"Отправить гет запрос ведите (1)" +
+    $"Выйти нажмите (2)");
+
+string answer = Console.ReadLine();
+
+    if (answer == "1")
+    {
+    Console.WriteLine(JToken.Parse(responseBody).ToString());
+        
+    }
+    if (answer == "2")
+    {
+    Process.GetCurrentProcess().Kill();
+    }
+
+
+
+
+
