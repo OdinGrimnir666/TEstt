9a988fbcf62b499f0248be1dd372ab526d693421
THH THH <lingyr@yandex.ru>
2022-01-19 16:55:28 +0300

    TEst 2.0

--- TEstt/Class1.cs
+++ TEstt/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,24 @@ namespace TEstt
 {
     public  class Product
     {
+        [JsonProperty]
         public int Id { get; set; }
+        [JsonProperty]
         public string Name { get; set; }
+        [JsonProperty]
 
         public int CategoryId { get; set; }
-       
 
+        [JsonIgnoreAttribute]
+        public Categories Category { get; set; }
 
     }
 
     public class Categories
     {
+        [JsonProperty]
         public int Id { get; set; }
+        [JsonProperty]
         public string Name { get; set; }
 
     }
@@ -28,5 +35,5 @@ namespace TEstt
 
         public List <Product> Products { get; set; }
         public List <Categories> Categories { get; set; }
-    }
+
 }
--- TEstt/Program.cs
+++ TEstt/Program.cs
@@ -13,56 +13,63 @@ response.EnsureSuccessStatusCode();
 return await response.Content.ReadAsStringAsync();
 }
 
-void ShowModel(Json Json)
-{
-    Console.WriteLine("Products");
-    foreach (var item in Json.Products)
-    {
-        Console.WriteLine("------------------");
-        Console.WriteLine($"Id:{item.Id}");
-        Console.WriteLine($"Name:{item.Name}");
-        Console.WriteLine($"CategoryId:{item.CategoryId}");
-        Console.WriteLine("----------------------");
-    };
-    Console.WriteLine("Categories");
-    foreach (var item in Json.Categories)
-    {
-        Console.WriteLine("----------------------");
-        Console.WriteLine($"Name:{item.Name}");
-        Console.WriteLine($"Id:{item.Id}");
-        Console.WriteLine("-------------------");
-    }
-}
+
 
 
 string responseBody = JSonAsync().Result;
 //Console.WriteLine(responseBody);
 Json account = JsonConvert.DeserializeObject<Json>(responseBody);
 
-string json = JsonConvert.SerializeObject(account, Formatting.Indented);
 
 
-while (true)
+foreach (var prod in account.Products)
+    prod.Category = account.Categories.FirstOrDefault(x => x.Id == prod.CategoryId);
+
+//string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+void Show()
 {
-    Console.WriteLine("1=Десерилизовать ,2= Серелизировать,3=сотановить");
-    string answer = Console.ReadLine();
-
-    if (answer == "1")
+foreach (var item in account.Products)
+{
+    var json1 = new Json()
     {
-        ShowModel(account);
+        Products = new List<Product>()
+        {
+             new Product()
+             {
+                 Id=item.Id,
+                 Name=item.Name,
+                 CategoryId=item.CategoryId
+             }
 
-
-    }
-    else if (answer == "2")
-    {
-        Console.WriteLine(json);
-    }
-    else if(answer=="3")
-    {
-        Environment.Exit(0);
-
-    }
+        },
+        Categories=new List<Categories>()
+        {
+            new Categories()
+            {
+                Id=item.Category.Id,
+                Name=item.Category.Name
+            }
+        }
+        
+    };
+    string jsondes = JsonConvert.SerializeObject(json1, Formatting.Indented);
+    Console.WriteLine(jsondes);
 }
+
+}
+Console.WriteLine("Поазать 1 ,Закрыть 2");
+string answer = Console.ReadLine();
+
+
+
+if (answer == "1")
+    Show();
+else if(answer=="2")
+    Environment.Exit(0);
+
+
+
 
 
 
